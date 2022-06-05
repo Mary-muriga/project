e4e3a39999cff10490312c915259f2f527c299c3
Marymuriga <marymuriga@gmail.com>
2022-06-06 01:46:01 +0300

    Editing my navbar

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const h2 = document.createElement("h2");
-document.querySelector("body").appendChild(h2);
+const links = document.querySelecttorAll(".link");
+const sections = document.querySelector("section");
+     let activeLink = 0;
+
+ links.forEach((link, i) => {
+  link.addEventListener('click', () => {
+    if(activeLink != i){
+     links[activeLink].classList.remove('active');
+     link.classList.add('active');
+      sections[activeLink].classList.remove('active');
+      
+     setTimeout(() => {
+        activeLink = i;
+        sections[i].classList.add('active');
+        }, 1000);
+    }
+
+ })
+
+})
